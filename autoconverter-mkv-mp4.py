e496b54ac00063155daf724ae0586f9e154d85df
Viktor Holovin <holovin@mail.com>
2021-09-25 16:35:49 +0200

    Language choose

--- autoconverter-mkv-mp4.py
+++ autoconverter-mkv-mp4.py
@@ -2,10 +2,12 @@ import os
 import subprocess
 
 folder_path = input("Enter your folder path with .mkv files: ")
+language = input("Enter your language (Example: eng, ita): ")
 for root, dirs, files in os.walk(folder_path):
     for file in files:
         if file.endswith(".mkv"):
             if file.replace(".mkv", ".mp4") in files:
                 continue
             else:
-                subprocess.run(["ffmpeg", "-i", root+"/"+file, root+"/"+file.replace(".mkv", ".mp4")])
+                subprocess.run(["ffmpeg", "-i", root+"/"+file, "-metadata:s:a:0", "language="+l
+anguage, root+"/"+file.replace(".mkv", ".mp4")])
